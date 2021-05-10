146a7acdfdd01a2b95a44d6d4473a530fc7dc521
SardorMS <mshinodawindows@gmail.com>
2021-05-10 22:22:03 +0500

    third commit

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,28 +5,36 @@ import (
 	"io"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
 )
 
+const lineBreaker string = "\r\n"
+
+//HandleFunc - ...
 type HandlerFunc func(conn net.Conn)
 
+//Server - Servers struct.
 type Server struct {
 	addr     string
 	mu       sync.RWMutex
 	handlers map[string]HandlerFunc
 }
 
+//NewServer - create server method.
 func NewServer(addr string) *Server {
 	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
 }
 
+//Register - Register the connection.
 func (s *Server) Register(path string, handler HandlerFunc) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.handlers[path] = handler
 }
 
+//Start - starts server.
 func (s *Server) Start() error {
 	listener, err := net.Listen("tcp", s.addr)
 	if err != nil {
@@ -53,18 +61,16 @@ func (s *Server) Start() error {
 	}
 }
 
+//handle - handles the connection
 func (s *Server) handle(conn net.Conn) {
 
 	defer func() {
-		err := conn.Close()
-		if err != nil {
-			log.Print("Error handle():", err)
-			return
+		if cerr := conn.Close(); cerr != nil {
+			log.Println(cerr)
 		}
 	}()
 
 	buf := make([]byte, 4096)
-
 	n, err := conn.Read(buf)
 	if err == io.EOF {
 		log.Printf("Error EOF: %s", buf[:n])
@@ -92,7 +98,12 @@ func (s *Server) handle(conn net.Conn) {
 		return
 	}
 
-	method, path := parts[0], parts[1]
+	method, path, version := parts[0], parts[1], parts[2]
+
+	if version != "HTTP/1.1" {
+		log.Print("Version Not HTTP/1.1: ", version)
+		return
+	}
 
 	if method != "GET" {
 		log.Print("Method Not GET: ", method)
@@ -104,5 +115,13 @@ func (s *Server) handle(conn net.Conn) {
 		s.mu.RUnlock()
 		handler(conn)
 	}
+}
 
+//Responce - response to request.
+func (s *Server) Response(body string) string {
+	return "HTTP/1.1 200 OK\r\n" +
+		"Content-Length: " + strconv.Itoa(len(body)) + lineBreaker +
+		"Content-Type: text/html\r\n" +
+		"Connection: close\r\n" +
+		lineBreaker + body
 }
